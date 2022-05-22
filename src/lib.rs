a0c01e1ffa3ab947c181f36d369ee0da12e1060a
TakeCare <takecare@pop-os.localdomain>
2022-05-22 17:04:16 +0200

    Refactor

---
+++ src/lib.rs
@@ -0,0 +1,146 @@
+use eframe::egui;
+use livesplit_core::TimeSpan;
+use livesplit_core::Timer;
+use livesplit_core::TimerPhase::*;
+use std::sync::{Arc, RwLock};
+
+pub struct Speedrun {
+    name: String,
+    timer: Arc<RwLock<Timer>>,
+    splits: Arc<RwLock<Splits>>,
+}
+
+impl Speedrun {
+    pub fn new(name: String, timer: Arc<RwLock<Timer>>, splits: Arc<RwLock<Splits>>) -> Self {
+        Self {
+            name,
+            timer,
+            splits,
+        }
+    }
+
+    /// Get name of speedrun application (window title)
+    pub fn get_name(&self) -> String {
+        self.name.clone()
+    }
+}
+
+#[derive(Default)]
+struct Split {
+    name: String,
+    time: TimeSpan,
+}
+
+#[derive(Default)]
+pub struct Splits {
+    splits: Vec<Split>,
+    name_padding: usize,
+}
+
+impl eframe::App for Speedrun {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let splits = self.splits.read().unwrap();
+        let timespan = self
+            .timer
+            .read()
+            .unwrap()
+            .snapshot()
+            .current_time()
+            .real_time
+            .unwrap();
+        let current_time = format_timespan(timespan);
+        let padding = splits.name_padding;
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading(self.name.clone());
+            for i in 0..splits.len() {
+                ui.horizontal(|ui| {
+                    ui.monospace(format!("{:<padding$}:", splits.get_split_name(i)));
+                    ui.monospace(splits.get_timespan(i));
+                });
+            }
+            ui.horizontal(|ui| {
+                ui.monospace(format!("{:<padding$}:", "Time"));
+                ui.monospace(current_time);
+            });
+        });
+
+        // continously repaint even if out of focus
+        ctx.request_repaint();
+    }
+}
+
+impl Splits {
+    pub fn new(split_names: Vec<String>) -> Splits {
+        let mut splits: Vec<Split> = Vec::new();
+        for name in split_names.clone() {
+            splits.push(Split {
+                name,
+                time: TimeSpan::default(),
+            });
+        }
+
+        Splits {
+            splits,
+            // padding for names of splits (= longest name)
+            name_padding: split_names.into_iter().map(|n| n.len()).max().unwrap_or(0),
+        }
+    }
+
+    /// Updates timespan for split i
+    fn update_timespan(&mut self, i: usize, timespan: TimeSpan) {
+        self.splits[i].time = timespan;
+    }
+
+    /// Get name of split i
+    fn get_split_name(&self, i: usize) -> String {
+        self.splits[i].name.clone()
+    }
+
+    /// Get timespan of split i
+    fn get_timespan(&self, i: usize) -> String {
+        self.splits[i].format_timespan()
+    }
+
+    /// Returns the number of splits
+    fn len(&self) -> usize {
+        self.splits.len()
+    }
+}
+
+impl Split {
+    /// Formats timespan to hh:mm:ss.ms
+    fn format_timespan(&self) -> String {
+        format_timespan(self.time)
+    }
+}
+
+/// Formats timespan to hh:mm:ss.ms
+pub fn format_timespan(timespan: TimeSpan) -> String {
+    let d = timespan.to_duration();
+    format!(
+        "{:02}:{:02}:{:02}.{:04}",
+        d.whole_hours(),
+        d.whole_minutes(),
+        d.whole_seconds(),
+        d.whole_milliseconds()
+    )
+}
+
+/// Starts the timer with the relevant keybinding and prints a message ("Start!" or "Split!")
+pub fn start_or_split_timer(timer: Arc<RwLock<Timer>>, splits: Arc<RwLock<Splits>>) {
+    let mut splits = splits.write().unwrap();
+    let message = match timer.read().unwrap().current_phase() {
+        NotRunning => "Start!",
+        _ => "Split!",
+    };
+    println!("{message}");
+    timer.write().unwrap().split_or_start();
+    let timer = timer.read().unwrap();
+    let snapshot = timer.snapshot();
+    let segments = snapshot.run().segments();
+    for (i, segment) in segments.iter().enumerate() {
+        if let Some(timespan) = segment.split_time().real_time {
+            splits.update_timespan(i, timespan);
+        };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,150 +1,32 @@
-use eframe::egui;
 use inputbot::KeybdKey::Numpad1Key;
-use inputbot::KeybdKey::Numpad3Key;
-use livesplit_core::TimerPhase::*;
 use livesplit_core::{Run, Segment, Timer};
+use speedrun_splits::{start_or_split_timer, Speedrun, Splits};
 use std::sync::{Arc, RwLock};
 use std::thread;
 
-struct MyApp {
-    name: String,
-    timer: Arc<RwLock<Timer>>,
-    splits: Arc<RwLock<[String; 5]>>,
-}
-
-impl MyApp {
-    pub fn new(name: String, timer: Arc<RwLock<Timer>>, splits: Arc<RwLock<[String; 5]>>) -> Self {
-        Self {
-            name,
-            timer,
-            splits,
-        }
-    }
-}
-
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let splits = self.splits.read().unwrap();
-        println!("{splits:?}");
-        let current_time = {
-            let d = self
-                .timer
-                .read()
-                .unwrap()
-                .snapshot()
-                .current_time()
-                .real_time
-                .unwrap()
-                .to_duration();
-            format!(
-                "{}h{}m{}.{}",
-                d.whole_hours(),
-                d.whole_minutes(),
-                d.whole_seconds(),
-                d.whole_milliseconds()
-            )
-        };
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading(self.name.clone());
-            ui.horizontal(|ui| {
-                ui.label("Tartarus: ");
-                ui.label(splits[0].to_string());
-            });
-            ui.horizontal(|ui| {
-                ui.label("Asphodel: ");
-                ui.label(splits[1].to_string());
-            });
-            ui.horizontal(|ui| {
-                ui.label("Elysium: ");
-                ui.label(splits[2].to_string());
-            });
-            ui.horizontal(|ui| {
-                ui.label("Styx: ");
-                ui.label(splits[3].to_string());
-            });
-            ui.horizontal(|ui| {
-                ui.label("Hades: ");
-                ui.label(splits[4].to_string());
-            });
-            ui.horizontal(|ui| {
-                ui.label("Time: ");
-                ui.label(current_time);
-            });
-        });
-
-        // continously repaint even if out of focus
-        ctx.request_repaint();
-    }
-}
-
-/// Starts the timer with the relevant keybinding and prints a message
-fn start_or_split_timer(timer: Arc<RwLock<Timer>>, splits: Arc<RwLock<[String; 5]>>) {
-    let mut splits = splits.write().unwrap();
-    let message = match timer.read().unwrap().current_phase() {
-        // FIXME wrong start message
-        Running | Ended => "Start!",
-        _ => "Split!",
-    };
-    println!("{}", message);
-    timer.write().unwrap().split_or_start();
-    let timer = timer.read().unwrap();
-    let snapshot = timer.snapshot();
-    let segments = snapshot.run().segments();
-    for i in 0..5 {
-        if let Some(real_time) = segments[i].split_time().real_time {
-            let d = real_time.to_duration();
-            splits[i] = format!(
-                "{}h{}m{}.{}",
-                d.whole_hours(),
-                d.whole_minutes(),
-                d.whole_seconds(),
-                d.whole_milliseconds()
-            );
-        };
-    }
-    println!("split: {splits:?}");
-}
-
-/// Prints current timer and split
-fn print_timer(timer: Arc<RwLock<Timer>>) {
-    let timer = timer.read().unwrap();
-    let d = timer
-        .snapshot()
-        .current_time()
-        .real_time
-        .unwrap()
-        .to_duration();
-    let phase = timer.current_split().unwrap().name();
-    println!(
-        "{}\t:{}h{}m{}.{}",
-        phase,
-        d.whole_hours(),
-        d.whole_minutes(),
-        d.whole_seconds(),
-        d.whole_milliseconds()
-    );
-}
-
 fn main() {
-    let splits: Arc<RwLock<[String; 5]>> = Default::default();
-    let splits_ref1: Arc<RwLock<[String; 5]>> = splits.clone();
+    let split_names: Vec<String> = vec![
+        "Tartarus".to_string(),
+        "Asphodel".to_string(),
+        "Elysium".to_string(),
+        "Styx".to_string(),
+        "Hades".to_string(),
+    ];
+    let splits: Arc<RwLock<Splits>> = Arc::new(RwLock::new(Splits::new(split_names.clone())));
+    let splits_ref1: Arc<RwLock<Splits>> = splits.clone();
 
     let mut run = Run::new();
     run.set_game_name("Hades");
     run.set_category_name("Clean file");
-    run.push_segment(Segment::new("Tartarus"));
-    run.push_segment(Segment::new("Asphodel"));
-    run.push_segment(Segment::new("Elysium"));
-    run.push_segment(Segment::new("Styx"));
-    run.push_segment(Segment::new("Hades"));
+    for name in split_names {
+        run.push_segment(Segment::new(name));
+    }
 
     // Arc allows any thread to point to some variable but it does not allow to
     // mutate it. This is why is wrapping a RwLock
     let t = Arc::new(RwLock::new(Timer::new(run).expect("")));
     let t1 = t.clone();
-    let t2 = t.clone();
     Numpad1Key.bind(move || start_or_split_timer(t1.clone(), splits_ref1.clone()));
-    Numpad3Key.bind(move || print_timer(t2.clone()));
 
     // blocking statement can be handled by spawning it's own thread
     thread::spawn(move || {
@@ -153,11 +35,11 @@ fn main() {
     });
 
     let options = eframe::NativeOptions::default();
-    let app = MyApp::new("Poor man's LiveSplit".to_owned(), t, splits);
+    let app = Speedrun::new("Poor man's LiveSplit".to_owned(), t, splits);
 
     // also blocking
     eframe::run_native(
-        app.name.clone().as_str(),
+        app.get_name().as_str(),
         options,
         Box::new(|_cc| Box::new(app)),
     );
